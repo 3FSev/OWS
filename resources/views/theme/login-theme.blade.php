ee6153b09478a01275477b872aa02dbf52b3b3a9
eunc <eubertnovencido20@gmail.com>
2023-08-28 15:38:33 +0800

    package for login and register ui

--- resources/views/theme/login-theme.blade.php
+++ resources/views/theme/login-theme.blade.php
@@ -1,9 +1,13 @@
 <title>Ormeco Warehouse Management System</title>
 <link rel="icon" href="assets/ormeco-logo.png">
 <link rel="stylesheet"
-href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 <!-- Tempusdominus Bootstrap 4 -->
 <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+
 <!-- Theme style -->
 <link rel="stylesheet" href="dist/css/adminlte.min.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
