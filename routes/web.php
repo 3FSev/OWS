e203f87861add459d3a10674cad4118e90de2312
eunc <eubertnovencido20@gmail.com>
2023-09-21 14:01:32 +0800

    create route for every page and btn in manager

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ManagerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuperUserController;
 
@@ -14,7 +15,7 @@ use App\Http\Controllers\SuperUserController;
 |
 */
 
-
+// Register
 Route::get('/register', function () {
     return view('auth.register');
 });
@@ -23,6 +24,7 @@ Route::get('/dashboard', function () {
     return view('superuser.sup-dashboard');
 });
 
+// <route for superuser>
 Route::get('/superuser/sup-dashboard', [SuperUserController::class, 'Dashboard'])->name('dashboard.sup');
 Route::get('/superuser/sup-create-user', [SuperUserController::class, 'CreateUser'])->name('create.sup');
 Route::get('/superuser/sup-unverified-user', [SuperUserController::class, 'UnverifiedUser'])->name('unverified.sup');
@@ -32,4 +34,15 @@ Route::get('/superuser/sup-manage-districts', [SuperUserController::class, 'Mana
 Route::get('/superuser/sup-user-activities', [SuperUserController::class, 'UserActivities'])->name('userActivities.sup');
 Route::get('/superuser/sup-restore-item', [SuperUserController::class, 'RestoreItem'])->name('restoreItems.sup');
 Route::get('/superuser/sup-restore-accounts', [SuperUserController::class, 'RestoreItem'])->name('restoreAccounts.sup');
+//</ route for superuser >
 
+// <route for manager>
+Route::get('/man-dashboard', function () {
+    return view('manager.man-stock-list');
+});
+Route::get('/manager/man-stock-list', [ManagerController::class, 'StockList'])->name('stockList.man');
+Route::get('/manager/man-wiv-req', [ManagerController::class, 'WIVrequest'])->name('WivRequest.man');
+    Route::get('/manager/wiv-review', [ManagerController::class, 'WIVreview'])->name('WivReview.man');
+Route::get('/manager/man-mrt-req', [ManagerController::class, 'MRTrequest'])->name('MrtRequest.man');
+    Route::get('/manager/mrt-review', [ManagerController::class, 'MRTreview'])->name('MrtReview.man');
+// </route for manager>
