3206a3bc0a52326a7078ee3c16de8ce0fea3bf2f
eunc <eubertnovencido20@gmail.com>
2023-10-01 10:57:12 +0800

    separte account setting and change password

---
+++ resources/views/theme/account-setting.blade.php
@@ -0,0 +1,37 @@
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <!-- /.card-header -->
+          <div class="card-header card-header-custom">
+            <h5 class="m-0 text-bold">Account Details</h5>
+          </div>     
+            <div class="card-body">
+              <!-- /.form-group -->
+              <div class="form-group">
+                  <label>Full Name</label>
+                  <input class="form-control" type="text" placeholder="Full Name">
+              </div>
+              <div class="form-group">
+                  <label>Email</label>
+                  <input type="email" class="form-control" style="width: 100%;" placeholder="Email Address">
+              </div>
+              <div class="form-group">
+                  <label>Department</label>
+                  <input type="Password" class="form-control" style="width: 100%;" placeholder="Password">
+              </div>
+              <!-- /.form-group -->
+              <div class="d-flex justify-content-end mt-5"> 
+                  <button class="btn bg-warning edit-btn text-white text-bold mr-2"><i class="fas fa-pencil-alt text-white mr-1"></i> Edit</button>
+                  <button class="btn bg-success text-white text-bold toastUpdateAccDetails"><i class="fas fa-check mr-1"></i> Update</button>
+              </div>
+            </div>
+          </div>
+          <!-- /.card-body -->
+        </div>
+     </div><!-- /.container-fluid -->
+  </div><!-- /.container-fluid -->
+</section>
---
+++ resources/views/theme/change-password.blade.php
@@ -0,0 +1,36 @@
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <!-- /.card-header -->
+          <div class="card-header card-header-custom">
+            <h5 class="m-0 text-bold">Password Details</h5>
+          </div>     
+          <div class="card-body">
+            <!-- /.form-group -->
+            <div class="form-group">
+                <label>Old Password</label>
+                <input class="form-control" type="text" placeholder="Old Password">
+            </div>
+            <div class="form-group">
+                <label>New Password</label>
+                <input type="email" class="form-control" style="width: 100%;" placeholder="Email Address">
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="Password" class="form-control" style="width: 100%;" placeholder="Password">
+            </div>
+            <!-- /.form-group -->
+            <div class="d-flex justify-content-end mt-5">
+                <button class="btn bg-primary text-white text-bold toastChangePassword"><i class="fas fa-check mr-1"></i> Save</button>
+            </div>
+          </div>
+          </div>
+          <!-- /.card-body -->
+        </div>
+      </div><!-- /.container-fluid -->
+  </div>
+</section>
