753a0e43bbb942438d8948070e8deeed6d2a5ad2
eunc <eubertnovencido20@gmail.com>
2023-12-03 20:17:15 +0800

    add create categories and routes

---
+++ resources/views/Admin/adm-create-items-categories.blade.php
@@ -0,0 +1,300 @@
+<html>
+@include('theme/plugins-theme')
+@include('theme/preloader')
+@include('theme/navbar')
+
+<title>Dashboard</title>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-green elevation-4">
+      <!-- Brand Logo -->
+      <div class="d-flex align-items-center">
+        <a href="#" class="brand-link" style="display: flex; align-items: center;">
+          <img src="{{ asset('assets/ormeco-logo.png') }}" alt="Ormeco Logo"
+            class="brand-image-xl img-circle elevation-3">
+          <div class="brand-text-custom">
+            <span class="brand-text font-weight-light">Warehouse<br>Management System</span>
+          </div>
+        </a>
+      </div>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item ">
+              <a href="{{ route('Dashboard.adm') }}" class="nav-link">
+                <i class="nav-icon fas fa-home nav-icon"></i>
+                <p>
+                  Dashboard
+                </p>
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a href="{{ route('Employee.adm') }}" class="nav-link ">
+                <i class="nav-icon fa-solid fa-user-group nav-icon"></i>
+                <p>
+                  Employee List
+                </p>
+              </a>
+            </li>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('CreateWIV.adm') }}" class="nav-link">
+                <i class="nav-icon fa-solid fa-ticket nav-icon"></i>
+                <p>
+                  Manage WIV
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a  href="{{ route('CreateMRT.adm') }}" class="nav-link">
+                <i class="nav-icon fa-solid fa-ticket-simple nav-icon"></i>
+                <p>
+                  Manage MRT
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('ItemList.adm') }}" class="nav-link active">
+                <i class="nav-icon fas fa-box-archive nav-icon "></i>
+                <p>
+                  Manage Stock
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="{{ route('CreateRR.adm') }}" class="nav-link ">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>Receiving Report</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('CreateItemCategories.adm') }}" class="nav-link active">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>Create Category</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('ItemList.adm') }}" class="nav-link ">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>Item List</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">
+                <i class="nav-icon fa-solid fa-file-lines nav-icon"></i>
+                <p>
+                  Manage Request    
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="{{ route('ItemRequest.adm') }}" class="nav-link">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>Item Request</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('ReturnItemRequest.adm') }}" class="nav-link">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>Return Item Request</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">
+                <i class="nav-icon fas fa-chart-pie nav-icon"></i>
+                <p>
+                  Reports
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="{{ route('WIVReports.adm') }}" class="nav-link">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>WIV</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('MRTReports.adm') }}" class="nav-link">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>MRT</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('RRReports.adm') }}" class="nav-link">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p>RR</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Create Item Categories</h1>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h2 class="card-title">Categories of Item List</h2>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
+                                <i class="fas fa-plus-circle" style="color: #ffffff;"></i>&nbsp;
+                                <span>Add</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="modal-default">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Add New Category</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form class="container-fluid" autocomplete="off">
+                                  @csrf
+                                  <div class="modal-body">
+                                    <div class="form-group">
+                                      <label class="col-form-label-md">Category Name</label>
+                                    <input class="form-control" name="district" type="text" placeholder="Category Name">
+                                    </div>
+                                  </div>
+                                  <div class="modal-footer justify-content-between">
+                                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                      <button type="submit" class="btn btn-primary">Confirm</button>
+                                  </div>
+                                </form>
+                            </div>
+                            <!-- /.modal-content -->
+                        </div>
+                        <!-- /.modal-dialog -->
+                    </div>
+                </div>
+  
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <table id="example2" class="table table-bordered table-hover">
+                      <thead>
+                      <tr>
+                        <th class="text-left">Name</th>
+                        <th class="text-left">Action</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                        <tr data-id="">
+                            <td>Sample</td>
+                            <td>
+                                <div class="text-center">
+                                    <button type="button" class="btn btn-warning btn-edit" data-toggle="modal" data-target="#modal-edit">
+                                        <i class="fas fa-pencil-alt" style="color: white;"></i>
+                                    </button>
+                                    <form method="POST" action="" style="display:inline;">
+                                      @csrf
+                                      @method('DELETE')
+                                      <button onclick="return confirm('Are You Sure')" type="submit" class="btn btn-danger">
+                                          <i class="fas fa-trash"></i>
+                                      </button>
+                                  </form>
+                                </div>
+                            </td>
+                        </tr>
+                        {{--  @endforeach  --}}
+                      </tbody>
+                    </table>
+                    <div class="modal fade" id="modal-edit">
+                      <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                                  <h4 class="modal-title">Edit District Name</h4>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                  </button>
+                              </div>
+                              <form id="editForm" method="POST" action="{{ route('district.update', ['id' => '__district_id__']) }}">
+                                @csrf
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label class="col-form-label-md">District Name</label>
+                                        <input name="districtName" id="districtNameInput" class="form-control" type="text" placeholder="District Name">
+                                    </div>
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Confirm</button>
+                                </div>
+                            </form>
+                          </div>
+                          <!-- /.modal-content -->
+                      </div>
+                      <!-- /.modal-dialog -->
+                  </div>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+              </div>
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+  </div>
+  <script>
+    $(function () {
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        "columnDefs": [
+          {
+              "targets": [1], // index of the "Action" column
+              "width": "150px" // set the desired width
+          }
+      ]
+      });
+    });
+  </script>
+</body>
+</html>
