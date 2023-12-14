46e69872d7978185206b62978438e4cc2e18b3c0
eunc <eubertnovencido20@gmail.com>
2023-12-14 21:48:12 +0800

    restict future date in datepicker

---
+++ public/plugins/datepicker/datepicker.js
@@ -0,0 +1,129 @@
+// $(document).ready(function () {
+//     // Initialize the date picker
+//     $('#datepicker1').datepicker({
+//         autoclose: true,
+//         endDate: "today",
+//         maxDate: today,
+//     });
+//     $('#datepicker-icon1').on('click', function () {
+//         $('#datepicker1').datepicker('show');
+//     });
+// });
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker1').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+
+    // Show the datepicker when the calendar icon is clicked
+    $('#datepicker-icon1').on('click', function () {
+        $('#datepicker1').datepicker('show');
+    });
+});
+
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker2').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+
+    $('#datepicker-icon2').on('click', function () {
+        $('#datepicker2').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker3').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+
+    $('#datepicker-icon3').on('click', function () {
+        $('#datepicker3').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker4').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+    $('#datepicker-icon4').on('click', function () {
+        $('#datepicker4').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker5').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+    $('#datepicker-icon5').on('click', function () {
+        $('#datepicker5').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker6').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+    $('#datepicker-icon6').on('click', function () {
+        $('#datepicker6').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker7').datepicker({
+        autoclose: true,
+        endDate: today
+    });s
+    $('#datepicker-icon7').on('click', function () {
+        $('#datepicker7').datepicker('show');
+    });
+});
+
+$(document).ready(function () {
+    // Get today's date
+    var today = new Date();
+
+    // Initialize the date picker with endDate attribute
+    $('#datepicker8').datepicker({
+        autoclose: true,
+        endDate: today
+    });
+    $('#datepicker-icon8').on('click', function () {
+        $('#datepicker8').datepicker('show');
+    });
+});
+
