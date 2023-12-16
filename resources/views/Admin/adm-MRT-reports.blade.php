79f866a27efecc6a820cad6d5b5be5c777a0c4eb
eunc <eubertnovencido20@gmail.com>
2023-12-16 23:08:28 +0800

    change search into filter

--- resources/views/Admin/adm-MRT-reports.blade.php
+++ resources/views/Admin/adm-MRT-reports.blade.php
@@ -240,7 +240,10 @@
           title: 'Warehouse Issued Voucher Reports',
           filename: 'wiv_report',
         },
-      ]
+      ],
+      "language": {
+        "search": "Filter"
+      }
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
   
     $('#example2').DataTable({
